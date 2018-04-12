6c745cb0990ddb14aace5d79790d950e53f0d139
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-04-13 07:22:31 +0900

    Display some graphes

--- titanic/protos/research.py
+++ titanic/protos/research.py
@@ -2,11 +2,23 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# x = np.arange(-3, 3, 0.1)
-# y = np.sin(x)
-# plt.plot(x, y)
-# plt.show()
-df = pd.read_csv('./input/train.csv')
-print(df.dtypes)
-print(df.head(3))
-print(df.describe())
+# df = pd.read_csv('./input/train.csv')
+# print(df.dtypes)
+# print(df.head(3))
+# print(df.describe())
+
+# Cleaning
+df= pd.read_csv('./input/train.csv').replace('male', 0).replace('female', 1)
+
+# Visualize
+split_data = []
+for survived in [0, 1]:
+    split_data.append(df[df.Survived == survived])
+
+temp = [i["Pclass"].dropna() for i in split_data]
+plt.hist(temp, histtype="barstacked", bins=3)
+plt.show()
+
+temp = [i["Age"].dropna() for i in split_data]
+plt.hist(temp, histtype="barstacked", bins=16)
+plt.show()
