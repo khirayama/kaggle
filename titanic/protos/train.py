a1ecbe4f6bc793837166896cb86c6d5b58437b49
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-04-16 08:15:31 +0900

    Create train.py

---
+++ titanic/protos/train.py
@@ -0,0 +1,42 @@
+import csv
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+
+# df = pd.read_csv('./input/train.csv')
+# print(df.dtypes)
+# print(df.head(3))
+# print(df.describe())
+
+df= pd.read_csv('./input/train.csv').replace('male', 0).replace('female', 1)
+df['Age'].fillna(df.Age.median(), inplace = True)
+df['FamilySize'] = df['SibSp'] + df['Parch'] + 1
+df2 = df.drop(['Name', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'], axis = 1)
+
+train_data = df2.values
+xs = train_data[:, 2:]
+y = train_data[:, 1]
+
+forest = RandomForestClassifier(n_estimators = 100)
+
+forest = forest.fit(xs, y)
+
+test_df = pd.read_csv('./input/test.csv').replace('male', 0).replace('female', 1)
+
+test_df['Age'].fillna(df.Age.median(), inplace = True)
+test_df['FamilySize'] = df['SibSp'] + df['Parch'] + 1
+test_df2 = test_df.drop(['Name', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'], axis = 1)
+
+test_data = test_df2.values
+xs_test = test_data[:, 1:]
+output = forest.predict(xs_test)
+
+zip_data = zip(test_data[:,0].astype(int), output.astype(int))
+predict_data = list(zip_data)
+
+with open('predict_result_data.csv', 'w') as f:
+    writer = csv.writer(f, lineterminator='\n')
+    writer.writerow(['PassengerId', 'Survived'])
+    for pid, survived in zip(test_data[:,0].astype(int), output.astype(int)):
+        writer.writerow([pid, survived])
