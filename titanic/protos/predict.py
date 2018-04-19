3b26ea0ba6b690900de79c87d1c37fdbf15695b3
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-04-19 18:24:33 +0900

    Update predict.py

--- titanic/protos/predict.py
+++ titanic/protos/predict.py
@@ -5,10 +5,18 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 
 df_train = pd.read_csv('./input/train.csv')
-df_train = df_train.replace('male', 0).replace('female', 1)
+
 df_train['Age'].fillna(df_train.Age.median(), inplace = True)
-df_train['FamilySize'] = df_train['SibSp'] + df_train['Parch'] + 1
-train_data = df_train.drop(['Name', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'], axis = 1).values
+
+sex_dum = pd.get_dummies(df_train['Sex'])
+df_train_proc = pd.concat((df_train, sex_dum), axis = 1)
+df_train_proc = df_train_proc.drop('Sex', axis = 1)
+
+emb_dum = pd.get_dummies(df_train['Embarked'])
+df_train_proc = pd.concat((df_train_proc, emb_dum), axis = 1)
+df_train_proc = df_train_proc.drop('Embarked', axis = 1)
+
+train_data = df_train_proc.drop(['Name', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin'], axis = 1).values
 
 xs = train_data[:, 2:]
 y = train_data[:, 1]
@@ -16,11 +24,19 @@ y = train_data[:, 1]
 forest = RandomForestClassifier(n_estimators = 100)
 forest = forest.fit(xs, y)
 
-test_df = pd.read_csv('./input/test.csv')
-test_df = test_df.replace('male', 0).replace('female', 1)
-test_df['Age'].fillna(df_train.Age.median(), inplace = True)
-test_df['FamilySize'] = df_train['SibSp'] + df_train['Parch'] + 1
-test_data = test_df.drop(['Name', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'], axis = 1).values
+df_test = pd.read_csv('./input/test.csv')
+
+df_test['Age'].fillna(df_train.Age.median(), inplace = True)
+
+sex_dum = pd.get_dummies(df_test['Sex'])
+df_test_proc = pd.concat((df_test, sex_dum), axis = 1)
+df_test_proc = df_test_proc.drop('Sex', axis = 1)
+
+emb_dum = pd.get_dummies(df_test['Embarked'])
+df_test_proc = pd.concat((df_test_proc, emb_dum), axis = 1)
+df_test_proc = df_test_proc.drop('Embarked', axis = 1)
+
+test_data = df_test_proc.drop(['Name', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin'], axis = 1).values
 
 xs_test = test_data[:, 1:]
 output = forest.predict(xs_test)
